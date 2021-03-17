f1921506a9f5b4617668017396858895c77b3913
Aurora0002 <71129120+Aurora0002@users.noreply.github.com>
2021-03-17 19:05:59 +0100

    Endringer i script for kartet

--- hovedsiden/script.js
+++ hovedsiden/script.js
@@ -79,50 +79,81 @@ var fylkesinfo = [
 
     //Rogaland
     {TotaltS: "3 968", NyeS: "47", Per100kS: "79,4", Trend: "Stigende", Antallkom: "4", InnlagtIGår: "7", InnlagtNå: "7", TotaltDode: "18", DodeIDag: "0"},
+
+    //Norge
+    {TotaltS: "81 305", NyeS: "866", Per100kS: "177,7", Trend: "Stigende", Antallkom: "42", InnlagtIGår: "157 783", InnlagtNå: "170 912", TotaltDode: "640", DodeIDag: "0"}
 ];
 
+// Funkssjon som gir info for hele Norge 
+function Norge(){
+    fylke.innerHTML = "Norge"
+    totaltS.innerHTML = fylkesinfo[11].TotaltS;
+    nyeS.innerHTML = fylkesinfo[11].NyeS;
+    per100kS.innerHTML = fylkesinfo[11].Per100kS;
+    trend.innerHTML = fylkesinfo[11].Trend;
+    antallkom.innerHTML = fylkesinfo[11].Antallkom;
+    innlagtIGår.innerHTML = fylkesinfo[11].InnlagtIGår;
+    innlagtNå.innerHTML = fylkesinfo[11].InnlagtNå;
+    totaltDode.innerHTML = fylkesinfo[11].TotaltDode;
+    dodeIDag.innerHTML = fylkesinfo[11].DodeIDag;
+    if(fylkesinfo[11].Trend=="Stigende"){
+        fylke1.style.fill= "#F47C7C";
+        trend.src = "bilder/" + "pos_trend.png";
+    }
+    else if (fylkesinfo[11].Trend=="Flat"){
+        fylke1.style.fill= "#F7F48B";
+        trend.src = "bilder/" + "flat_trend.png";
+    }
+    else if (fylkesinfo[11].Trend=="Synkende"){
+        fylke1.style.fill= "#A1DE93";
+        trend.src = "bilder/" + "neg_trend.png";
+    }
+}
+Norge();
 
 // Gir tilbake info utifra byen du har trykket på
 
 var a = 1;
 fylke1.onclick = function(evt){
     event.preventDefault();
-    fylke.innerHTML = "Troms og Finnmark"
-    totaltS.innerHTML = fylkesinfo[0].TotaltS;
-    nyeS.innerHTML = fylkesinfo[0].NyeS;
-    per100kS.innerHTML = fylkesinfo[0].Per100kS;
-    trend.innerHTML = fylkesinfo[0].Trend;
-    antallkom.innerHTML = fylkesinfo[0].Antallkom;
-    innlagtIGår.innerHTML = fylkesinfo[0].InnlagtIGår;
-    innlagtNå.innerHTML = fylkesinfo[0].InnlagtNå;
-    totaltDode.innerHTML = fylkesinfo[0].TotaltDode;
-    dodeIDag.innerHTML = fylkesinfo[0].DodeIDag;
-   
+    fylke2.style.fill = "grey";
+    fylke3.style.fill = "grey";
+    fylke4.style.fill = "grey";
+    fylke5.style.fill = "grey";
+    fylke6.style.fill = "grey";
+    fylke7.style.fill = "grey";
+    fylke8.style.fill = "grey";
+    fylke9.style.fill = "grey";
+    fylke10.style.fill = "grey";
+    fylke11.style.fill = "grey";
+
+    b = 1;
+    c = 1;
+    d = 1;
+    e = 1;
+    f = 1;
+    g = 1;
+    h = 1;
+    i = 1;
+    j = 1;
+    k = 1;
+
     if (a%2 == 0){
+        Norge();
         fylke1.style.fill= "grey";
         a++;
     }
     else{
-        fylke2.style.fill = "grey";
-        fylke3.style.fill = "grey";
-        fylke4.style.fill = "grey";
-        fylke5.style.fill = "grey";
-        fylke6.style.fill = "grey";
-        fylke7.style.fill = "grey";
-        fylke8.style.fill = "grey";
-        fylke9.style.fill = "grey";
-        fylke10.style.fill = "grey";
-        fylke11.style.fill = "grey";
-        b = 1;
-        c = 1;
-        d = 1;
-        e = 1;
-        f = 1;
-        g = 1;
-        h = 1;
-        i = 1;
-        j = 1;
-        k = 1;
+        fylke.innerHTML = "Troms og Finnmark"
+        totaltS.innerHTML = fylkesinfo[0].TotaltS;
+        nyeS.innerHTML = fylkesinfo[0].NyeS;
+        per100kS.innerHTML = fylkesinfo[0].Per100kS;
+        trend.innerHTML = fylkesinfo[0].Trend;
+        antallkom.innerHTML = fylkesinfo[0].Antallkom;
+        innlagtIGår.innerHTML = fylkesinfo[0].InnlagtIGår;
+        innlagtNå.innerHTML = fylkesinfo[0].InnlagtNå;
+        totaltDode.innerHTML = fylkesinfo[0].TotaltDode;
+        dodeIDag.innerHTML = fylkesinfo[0].DodeIDag;
 
         if(fylkesinfo[0].Trend=="Stigende"){
             fylke1.style.fill= "#F47C7C";
@@ -143,17 +174,6 @@ fylke1.onclick = function(evt){
 var b = 1;
 fylke2.onclick = function(evt){
     event.preventDefault();
-    fylke.innerHTML = "Nordland"
-    totaltS.innerHTML = fylkesinfo[1].TotaltS;
-    nyeS.innerHTML = fylkesinfo[1].NyeS;
-    per100kS.innerHTML = fylkesinfo[1].Per100kS;
-    trend.innerHTML = fylkesinfo[1].Trend;
-    antallkom.innerHTML = fylkesinfo[1].Antallkom;
-    innlagtIGår.innerHTML = fylkesinfo[1].InnlagtIGår;
-    innlagtNå.innerHTML = fylkesinfo[1].InnlagtNå;
-    totaltDode.innerHTML = fylkesinfo[1].TotaltDode;
-    dodeIDag.innerHTML = fylkesinfo[1].DodeIDag;
-
     fylke1.style.fill = "grey";
     fylke3.style.fill = "grey";
     fylke4.style.fill = "grey";
@@ -164,22 +184,34 @@ fylke2.onclick = function(evt){
     fylke9.style.fill = "grey";
     fylke10.style.fill = "grey";
     fylke11.style.fill = "grey";
-        a = 1;
-        c = 1;
-        d = 1;
-        e = 1;
-        f = 1;
-        g = 1;
-        h = 1;
-        i = 1;
-        j = 1;
-        k = 1;
+
+    a = 1;
+    c = 1;
+    d = 1;
+    e = 1;
+    f = 1;
+    g = 1;
+    h = 1;
+    i = 1;
+    j = 1;
+    k = 1;
 
     if (b%2 == 0){
+        Norge();
         fylke2.style.fill= "grey";
         b++;
     }
     else{
+        fylke.innerHTML = "Nordland"
+        totaltS.innerHTML = fylkesinfo[1].TotaltS;
+        nyeS.innerHTML = fylkesinfo[1].NyeS;
+        per100kS.innerHTML = fylkesinfo[1].Per100kS;
+        trend.innerHTML = fylkesinfo[1].Trend;
+        antallkom.innerHTML = fylkesinfo[1].Antallkom;
+        innlagtIGår.innerHTML = fylkesinfo[1].InnlagtIGår;
+        innlagtNå.innerHTML = fylkesinfo[1].InnlagtNå;
+        totaltDode.innerHTML = fylkesinfo[1].TotaltDode;
+        dodeIDag.innerHTML = fylkesinfo[1].DodeIDag;
 
         if(fylkesinfo[1].Trend=="Stigende"){
             fylke2.style.fill= "#F47C7C";
@@ -199,17 +231,6 @@ fylke2.onclick = function(evt){
 var c = 1;
 fylke3.onclick = function(evt){
     event.preventDefault();
-    fylke.innerHTML = "Trøndelag"
-    totaltS.innerHTML = fylkesinfo[2].TotaltS;
-    nyeS.innerHTML = fylkesinfo[2].NyeS;
-    per100kS.innerHTML = fylkesinfo[2].Per100kS;
-    trend.innerHTML = fylkesinfo[2].Trend;
-    antallkom.innerHTML = fylkesinfo[2].Antallkom;
-    innlagtIGår.innerHTML = fylkesinfo[2].InnlagtIGår;
-    innlagtNå.innerHTML = fylkesinfo[2].InnlagtNå;
-    totaltDode.innerHTML = fylkesinfo[2].TotaltDode;
-    dodeIDag.innerHTML = fylkesinfo[2].DodeIDag;
-
     fylke1.style.fill = "grey";
     fylke2.style.fill = "grey";
     fylke4.style.fill = "grey";
@@ -233,10 +254,22 @@ fylke3.onclick = function(evt){
     k = 1;
 
     if (c%2 == 0){
+        Norge();
         fylke3.style.fill= "grey";
         c++;
     }
     else {
+        fylke.innerHTML = "Trøndelag"
+        totaltS.innerHTML = fylkesinfo[2].TotaltS;
+        nyeS.innerHTML = fylkesinfo[2].NyeS;
+        per100kS.innerHTML = fylkesinfo[2].Per100kS;
+        trend.innerHTML = fylkesinfo[2].Trend;
+        antallkom.innerHTML = fylkesinfo[2].Antallkom;
+        innlagtIGår.innerHTML = fylkesinfo[2].InnlagtIGår;
+        innlagtNå.innerHTML = fylkesinfo[2].InnlagtNå;
+        totaltDode.innerHTML = fylkesinfo[2].TotaltDode;
+        dodeIDag.innerHTML = fylkesinfo[2].DodeIDag;
+
         if(fylkesinfo[2].Trend=="Stigende"){
             fylke3.style.fill= "#F47C7C";
             trend.src = "bilder/" + "pos_trend.png";
@@ -257,17 +290,6 @@ fylke3.onclick = function(evt){
 var d = 1;
 fylke4.onclick = function(evt){
     event.preventDefault();
-    fylke.innerHTML = "Innlandet"
-    totaltS.innerHTML = fylkesinfo[3].TotaltS;
-    nyeS.innerHTML = fylkesinfo[3].NyeS;
-    per100kS.innerHTML = fylkesinfo[3].Per100kS;
-    trend.innerHTML = fylkesinfo[3].Trend;
-    antallkom.innerHTML = fylkesinfo[3].Antallkom;
-    innlagtIGår.innerHTML = fylkesinfo[3].InnlagtIGår;
-    innlagtNå.innerHTML = fylkesinfo[3].InnlagtNå;
-    totaltDode.innerHTML = fylkesinfo[3].TotaltDode;
-    dodeIDag.innerHTML = fylkesinfo[3].DodeIDag;
-
     fylke1.style.fill = "grey";
     fylke2.style.fill = "grey";
     fylke3.style.fill = "grey";
@@ -291,10 +313,22 @@ fylke4.onclick = function(evt){
     k = 1;
 
     if (d%2 == 0){
+        Norge();
         fylke4.style.fill= "grey";
         d++;
     }
     else {
+        fylke.innerHTML = "Innlandet"
+        totaltS.innerHTML = fylkesinfo[3].TotaltS;
+        nyeS.innerHTML = fylkesinfo[3].NyeS;
+        per100kS.innerHTML = fylkesinfo[3].Per100kS;
+        trend.innerHTML = fylkesinfo[3].Trend;
+        antallkom.innerHTML = fylkesinfo[3].Antallkom;
+        innlagtIGår.innerHTML = fylkesinfo[3].InnlagtIGår;
+        innlagtNå.innerHTML = fylkesinfo[3].InnlagtNå;
+        totaltDode.innerHTML = fylkesinfo[3].TotaltDode;
+        dodeIDag.innerHTML = fylkesinfo[3].DodeIDag;
+
         if(fylkesinfo[3].Trend=="Stigende"){
             fylke4.style.fill= "#F47C7C";
             trend.src = "bilder/" + "pos_trend.png";
@@ -315,17 +349,6 @@ fylke4.onclick = function(evt){
 var e =1;
 fylke5.onclick = function(evt){
     event.preventDefault();
-    fylke.innerHTML = "Oslo"
-    totaltS.innerHTML = fylkesinfo[4].TotaltS;
-    nyeS.innerHTML = fylkesinfo[4].NyeS;
-    per100kS.innerHTML = fylkesinfo[4].Per100kS;
-    trend.innerHTML = fylkesinfo[4].Trend;
-    antallkom.innerHTML = fylkesinfo[4].Antallkom;
-    innlagtIGår.innerHTML = fylkesinfo[4].InnlagtIGår;
-    innlagtNå.innerHTML = fylkesinfo[4].InnlagtNå;
-    totaltDode.innerHTML = fylkesinfo[4].TotaltDode;
-    dodeIDag.innerHTML = fylkesinfo[4].DodeIDag;
-
     fylke1.style.fill = "grey";
     fylke2.style.fill = "grey";
     fylke3.style.fill = "grey";
@@ -349,10 +372,22 @@ fylke5.onclick = function(evt){
     k = 1;
 
     if (e%2 == 0){
+        Norge();
         fylke5.style.fill= "grey";
         e++;
     }
     else {
+        fylke.innerHTML = "Oslo"
+        totaltS.innerHTML = fylkesinfo[4].TotaltS;
+        nyeS.innerHTML = fylkesinfo[4].NyeS;
+        per100kS.innerHTML = fylkesinfo[4].Per100kS;
+        trend.innerHTML = fylkesinfo[4].Trend;
+        antallkom.innerHTML = fylkesinfo[4].Antallkom;
+        innlagtIGår.innerHTML = fylkesinfo[4].InnlagtIGår;
+        innlagtNå.innerHTML = fylkesinfo[4].InnlagtNå;
+        totaltDode.innerHTML = fylkesinfo[4].TotaltDode;
+        dodeIDag.innerHTML = fylkesinfo[4].DodeIDag;
+
         if(fylkesinfo[4].Trend=="Stigende"){
             fylke5.style.fill= "#F47C7C";
             trend.src = "bilder/" + "pos_trend.png";
@@ -371,17 +406,6 @@ fylke5.onclick = function(evt){
 var f = 1;
 fylke6.onclick = function(evt){
     event.preventDefault();
-    fylke.innerHTML = "Viken"
-    totaltS.innerHTML = fylkesinfo[5].TotaltS;
-    nyeS.innerHTML = fylkesinfo[5].NyeS;
-    per100kS.innerHTML = fylkesinfo[5].Per100kS;
-    trend.innerHTML = fylkesinfo[5].Trend;
-    antallkom.innerHTML = fylkesinfo[5].Antallkom;
-    innlagtIGår.innerHTML = fylkesinfo[5].InnlagtIGår;
-    innlagtNå.innerHTML = fylkesinfo[5].InnlagtNå;
-    totaltDode.innerHTML = fylkesinfo[5].TotaltDode;
-    dodeIDag.innerHTML = fylkesinfo[5].DodeIDag;
-
     fylke1.style.fill = "grey";
     fylke2.style.fill = "grey";
     fylke3.style.fill = "grey";
@@ -405,10 +429,22 @@ fylke6.onclick = function(evt){
     k = 1;
 
     if (f%2 == 0){
+        Norge();
         fylke6.style.fill= "grey";
         f++;
     }
     else {
+        fylke.innerHTML = "Viken"
+        totaltS.innerHTML = fylkesinfo[5].TotaltS;
+        nyeS.innerHTML = fylkesinfo[5].NyeS;
+        per100kS.innerHTML = fylkesinfo[5].Per100kS;
+        trend.innerHTML = fylkesinfo[5].Trend;
+        antallkom.innerHTML = fylkesinfo[5].Antallkom;
+        innlagtIGår.innerHTML = fylkesinfo[5].InnlagtIGår;
+        innlagtNå.innerHTML = fylkesinfo[5].InnlagtNå;
+        totaltDode.innerHTML = fylkesinfo[5].TotaltDode;
+        dodeIDag.innerHTML = fylkesinfo[5].DodeIDag;
+
         if(fylkesinfo[5].Trend=="Stigende"){
             fylke6.style.fill= "#F47C7C";
             trend.src = "bilder/" + "pos_trend.png";
@@ -427,17 +463,6 @@ fylke6.onclick = function(evt){
 var g = 1;
 fylke7.onclick = function(evt){
     event.preventDefault();
-    fylke.innerHTML = "Vestfold og Telemark"
-    totaltS.innerHTML = fylkesinfo[6].TotaltS;
-    nyeS.innerHTML = fylkesinfo[6].NyeS;
-    per100kS.innerHTML = fylkesinfo[6].Per100kS;
-    trend.innerHTML = fylkesinfo[6].Trend;
-    antallkom.innerHTML = fylkesinfo[6].Antallkom;
-    innlagtIGår.innerHTML = fylkesinfo[6].InnlagtIGår;
-    innlagtNå.innerHTML = fylkesinfo[6].InnlagtNå;
-    totaltDode.innerHTML = fylkesinfo[6].TotaltDode;
-    dodeIDag.innerHTML = fylkesinfo[6].DodeIDag;
-
     fylke1.style.fill = "grey";
     fylke2.style.fill = "grey";
     fylke3.style.fill = "grey";
@@ -461,10 +486,22 @@ fylke7.onclick = function(evt){
     k = 1;
 
     if (g%2 == 0){
+        Norge();
         fylke7.style.fill= "grey";
         g++;
     }
     else {
+        fylke.innerHTML = "Vestfold og Telemark"
+        totaltS.innerHTML = fylkesinfo[6].TotaltS;
+        nyeS.innerHTML = fylkesinfo[6].NyeS;
+        per100kS.innerHTML = fylkesinfo[6].Per100kS;
+        trend.innerHTML = fylkesinfo[6].Trend;
+        antallkom.innerHTML = fylkesinfo[6].Antallkom;
+        innlagtIGår.innerHTML = fylkesinfo[6].InnlagtIGår;
+        innlagtNå.innerHTML = fylkesinfo[6].InnlagtNå;
+        totaltDode.innerHTML = fylkesinfo[6].TotaltDode;
+        dodeIDag.innerHTML = fylkesinfo[6].DodeIDag;
+
         if(fylkesinfo[6].Trend=="Stigende"){
             fylke7.style.fill= "#F47C7C";
             trend.src = "bilder/" + "pos_trend.png";
@@ -483,17 +520,6 @@ fylke7.onclick = function(evt){
 var h = 1;
 fylke8.onclick = function(evt){
     event.preventDefault();
-    fylke.innerHTML = "Møre og Romsdal"
-    totaltS.innerHTML = fylkesinfo[7].TotaltS;
-    nyeS.innerHTML = fylkesinfo[7].NyeS;
-    per100kS.innerHTML = fylkesinfo[7].Per100kS;
-    trend.innerHTML = fylkesinfo[7].Trend;
-    antallkom.innerHTML = fylkesinfo[7].Antallkom;
-    innlagtIGår.innerHTML = fylkesinfo[7].InnlagtIGår;
-    innlagtNå.innerHTML = fylkesinfo[7].InnlagtNå;
-    totaltDode.innerHTML = fylkesinfo[7].TotaltDode;
-    dodeIDag.innerHTML = fylkesinfo[7].DodeIDag;
-
     fylke1.style.fill = "grey";
     fylke2.style.fill = "grey";
     fylke3.style.fill = "grey";
@@ -517,10 +543,22 @@ fylke8.onclick = function(evt){
     k = 1;
 
     if (h%2 == 0){
+        Norge();
         fylke8.style.fill= "grey";
         h++;
     }
     else {
+        fylke.innerHTML = "Møre og Romsdal"
+        totaltS.innerHTML = fylkesinfo[7].TotaltS;
+        nyeS.innerHTML = fylkesinfo[7].NyeS;
+        per100kS.innerHTML = fylkesinfo[7].Per100kS;
+        trend.innerHTML = fylkesinfo[7].Trend;
+        antallkom.innerHTML = fylkesinfo[7].Antallkom;
+        innlagtIGår.innerHTML = fylkesinfo[7].InnlagtIGår;
+        innlagtNå.innerHTML = fylkesinfo[7].InnlagtNå;
+        totaltDode.innerHTML = fylkesinfo[7].TotaltDode;
+        dodeIDag.innerHTML = fylkesinfo[7].DodeIDag;
+
         if(fylkesinfo[7].Trend=="Stigende"){
             fylke8.style.fill= "#F47C7C";
             trend.src = "bilder/" + "pos_trend.png";
@@ -539,17 +577,6 @@ fylke8.onclick = function(evt){
 var i = 1;
 fylke9.onclick = function(evt){
     event.preventDefault();
-    fylke.innerHTML = "Vestland"
-    totaltS.innerHTML = fylkesinfo[8].TotaltS;
-    nyeS.innerHTML = fylkesinfo[8].NyeS;
-    per100kS.innerHTML = fylkesinfo[8].Per100kS;
-    trend.innerHTML = fylkesinfo[8].Trend;
-    antallkom.innerHTML = fylkesinfo[8].Antallkom;
-    innlagtIGår.innerHTML = fylkesinfo[8].InnlagtIGår;
-    innlagtNå.innerHTML = fylkesinfo[8].InnlagtNå;
-    totaltDode.innerHTML = fylkesinfo[8].TotaltDode;
-    dodeIDag.innerHTML = fylkesinfo[8].DodeIDag;
-
     fylke1.style.fill = "grey";
     fylke2.style.fill = "grey";
     fylke3.style.fill = "grey";
@@ -573,10 +600,22 @@ fylke9.onclick = function(evt){
     k = 1;
 
     if (i%2 == 0){
+        Norge();
         fylke9.style.fill= "grey";
         i++;
     }
     else {
+        fylke.innerHTML = "Vestland"
+        totaltS.innerHTML = fylkesinfo[8].TotaltS;
+        nyeS.innerHTML = fylkesinfo[8].NyeS;
+        per100kS.innerHTML = fylkesinfo[8].Per100kS;
+        trend.innerHTML = fylkesinfo[8].Trend;
+        antallkom.innerHTML = fylkesinfo[8].Antallkom;
+        innlagtIGår.innerHTML = fylkesinfo[8].InnlagtIGår;
+        innlagtNå.innerHTML = fylkesinfo[8].InnlagtNå;
+        totaltDode.innerHTML = fylkesinfo[8].TotaltDode;
+        dodeIDag.innerHTML = fylkesinfo[8].DodeIDag;
+
         if(fylkesinfo[8].Trend=="Stigende"){
             fylke9.style.fill= "#F47C7C";
             trend.src = "bilder/" + "pos_trend.png";
@@ -595,17 +634,6 @@ fylke9.onclick = function(evt){
 var j = 1;
 fylke10.onclick = function(evt){
     event.preventDefault();
-    fylke.innerHTML = "Agder"
-    totaltS.innerHTML = fylkesinfo[9].TotaltS;
-    nyeS.innerHTML = fylkesinfo[9].NyeS;
-    per100kS.innerHTML = fylkesinfo[9].Per100kS;
-    trend.innerHTML = fylkesinfo[9].Trend;
-    antallkom.innerHTML = fylkesinfo[9].Antallkom;
-    innlagtIGår.innerHTML = fylkesinfo[9].InnlagtIGår;
-    innlagtNå.innerHTML = fylkesinfo[9].InnlagtNå;
-    totaltDode.innerHTML = fylkesinfo[9].TotaltDode;
-    dodeIDag.innerHTML = fylkesinfo[9].DodeIDag;
-
     fylke1.style.fill = "grey";
     fylke2.style.fill = "grey";
     fylke3.style.fill = "grey";
@@ -629,10 +657,22 @@ fylke10.onclick = function(evt){
     k = 1;
 
     if (j%2 == 0){
+        Norge();
         fylke10.style.fill= "grey";
         j++;
     }
     else {
+        fylke.innerHTML = "Agder"
+        totaltS.innerHTML = fylkesinfo[9].TotaltS;
+        nyeS.innerHTML = fylkesinfo[9].NyeS;
+        per100kS.innerHTML = fylkesinfo[9].Per100kS;
+        trend.innerHTML = fylkesinfo[9].Trend;
+        antallkom.innerHTML = fylkesinfo[9].Antallkom;
+        innlagtIGår.innerHTML = fylkesinfo[9].InnlagtIGår;
+        innlagtNå.innerHTML = fylkesinfo[9].InnlagtNå;
+        totaltDode.innerHTML = fylkesinfo[9].TotaltDode;
+        dodeIDag.innerHTML = fylkesinfo[9].DodeIDag;
+
         if(fylkesinfo[9].Trend=="Stigende"){
             fylke10.style.fill= "#F47C7C";
             trend.src = "bilder/" + "pos_trend.png";
@@ -651,17 +691,6 @@ fylke10.onclick = function(evt){
 var k = 1;
 fylke11.onclick = function(evt){
     event.preventDefault();
-    fylke.innerHTML = "Rogaland"
-    totaltS.innerHTML = fylkesinfo[10].TotaltS;
-    nyeS.innerHTML = fylkesinfo[10].NyeS;
-    per100kS.innerHTML = fylkesinfo[10].Per100kS;
-    trend.innerHTML = fylkesinfo[10].Trend;
-    antallkom.innerHTML = fylkesinfo[10].Antallkom;
-    innlagtIGår.innerHTML = fylkesinfo[10].InnlagtIGår;
-    innlagtNå.innerHTML = fylkesinfo[10].InnlagtNå;
-    totaltDode.innerHTML = fylkesinfo[10].TotaltDode;
-    dodeIDag.innerHTML = fylkesinfo[10].DodeIDag;
-
     fylke1.style.fill = "grey";
     fylke2.style.fill = "grey";
     fylke3.style.fill = "grey";
@@ -685,10 +714,22 @@ fylke11.onclick = function(evt){
     j = 1;
 
     if (k%2 == 0){
+        Norge();
         fylke11.style.fill= "grey";
         k++;
     }
     else {
+        fylke.innerHTML = "Rogaland"
+        totaltS.innerHTML = fylkesinfo[10].TotaltS;
+        nyeS.innerHTML = fylkesinfo[10].NyeS;
+        per100kS.innerHTML = fylkesinfo[10].Per100kS;
+        trend.innerHTML = fylkesinfo[10].Trend;
+        antallkom.innerHTML = fylkesinfo[10].Antallkom;
+        innlagtIGår.innerHTML = fylkesinfo[10].InnlagtIGår;
+        innlagtNå.innerHTML = fylkesinfo[10].InnlagtNå;
+        totaltDode.innerHTML = fylkesinfo[10].TotaltDode;
+        dodeIDag.innerHTML = fylkesinfo[10].DodeIDag;
+        
         if(fylkesinfo[10].Trend=="Stigende"){
             fylke11.style.fill= "#F47C7C";
             trend.src = "bilder/" + "pos_trend.png";
